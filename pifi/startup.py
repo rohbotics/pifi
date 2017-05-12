a789a9b55cfa96db86efb3276503488b22a39e47
Rohan Agrawal <send2arohan@gmail.com>
2017-05-11 19:21:36 -0700

    add ability to load pending connections from file

--- pifi/startup.py
+++ pifi/startup.py
@@ -5,29 +5,40 @@ time.sleep(5)
 import NetworkManager
 
 import uuid
+import json
 
-def writeSeenSSIDs():
+def getSeenSSIDs(device):
     aps = dict()
 
-    for dev in NetworkManager.NetworkManager.GetDevices():
-        if dev.DeviceType != NetworkManager.NM_DEVICE_TYPE_WIFI:
-             continue
-        for ap in dev.SpecificDevice().GetAccessPoints():
-            aps[ap.Ssid] = ap
+    for ap in device.SpecificDevice().GetAccessPoints():
+        aps[ap.Ssid] = ap
 
+    return aps
+
+def writeSeenSSIDs(aps):
     target = open('/tmp/seen_ssids', 'w')
     target.truncate()
 
     for ssids in aps:
         target.write('%-30s \n' % (aps[ssids].Ssid))
 
+def readPendingConnections():
+    try:    
+        pending_file = open('/etc/pifi_pending', 'r')
+        return json.load(pending_file)
+    except IOError:
+        return dict()
 
 def checkCapablities(device_capabilities, capability):
     return device_capabilities & capability == capability
 
+def getAvailiblePendingConnection(seenSSIDs, pendingConnections):
+    for ssid in seenSSIDs:
+        for pcon in pendingConnections:
+            if ssid == pcon['ssid']:
+                return pcon
 
 def main():
-    writeSeenSSIDs()
     ApModeDevice = NetworkManager.Device
 
     for device in NetworkManager.NetworkManager.GetDevices():
@@ -44,6 +55,10 @@ def main():
         print "ERROR: Network Manager reports no AP mode support on any managed device"
         exit(2)
 
+    seenSSIDs = getSeenSSIDs(ApModeDevice)
+    writeSeenSSIDs(seenSSIDs)
+    pendingConnections = readPendingConnections()   
+ 
     # Allow 30 seconds for network manager to sort itself out
     time.sleep(30)
 
@@ -51,7 +66,45 @@ def main():
         print("Device currently connected to: %s" 
             % ApModeDevice.SpecificDevice().ActiveAccessPoint.Ssid)
     else:
-        print "Device is not connected to any network, Starting AP mode"
+        print "Device is not connected to any network, Looking for pending connections"
+
+        new_ap_connection = getAvailiblePendingConnection(seenSSIDs, pendingConnections)
+        if new_ap_connection is not None:
+            connection_uuid = str(uuid.uuid4())
+
+            settings = {
+                'connection': {
+                    'id': new_ap_connection['ssid'],
+                    'type': '802-11-wireless',
+                    'autoconnect': True,
+                    'uuid': connection_uuid
+                },
+
+                '802-11-wireless': {
+                    'mode': 'infrastructure',
+                    'security': '802-11-wireless-security',
+                    'ssid': new_ap_connection['ssid']
+                },
+
+                '802-11-wireless-security': {
+                    'key-mgmt': 'wpa-psk',
+                    'psk': new_ap_connection['password']
+                },
+
+                'ipv4': {'method': 'auto'},
+                'ipv6': {'method': 'auto'}
+            }
+            
+            print "Connecting to %s" % new_ap_connection['ssid']
+            NetworkManager.NetworkManager.AddAndActivateConnection(settings, ApModeDevice, "/")
+            
+            pendingConnections.remove(new_ap_connection) 
+            pending_file = open('/etc/pifi_pending', 'w')
+            pending_file.truncate()
+            pending_file.write(json.dumps(pendingConnections))
+            return
+		
+        print "No SSIDs assoicated with pending connections found, Starting AP mode"
 
         found_connection = False
         existing_connection = NetworkManager.Settings.Connection
