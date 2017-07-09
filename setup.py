c593135be134dccfcc2907817baad8a23a0379c8
Rohan Agrawal <send2arohan@gmail.com>
2017-07-09 11:29:54 -0700

    added pkla file, bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pifi',
-    version='0.1.0',
+    version='0.1.2',
     description='Wifi tools for the Raspberry Pi',
     url='https://github.com/rohbotics/pifi',
 
