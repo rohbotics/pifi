6ee500561f58a9e40f28256e5aee6ea79af76563
Rohan Agrawal <send2arohan@gmail.com>
2017-07-09 10:26:59 -0700

    Bump to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pifi',
-    version='0.0.3',
+    version='0.1.0',
     description='Wifi tools for the Raspberry Pi',
     url='https://github.com/rohbotics/pifi',
 
