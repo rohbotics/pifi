fff37cd7d75b825a4c95dccd8d083bde928e2418
Rohan Agrawal <send2arohan@gmail.com>
2017-07-22 18:22:09 -0700

    user helper modules in the cli code

--- pifi/pifi.py
+++ pifi/pifi.py
@@ -13,56 +13,41 @@ Options:
   --version    Show pifi version
 
 """
-from __future__ import print_function
 from docopt import docopt
 
 import NetworkManager
 import json
 import os
 
-def checkCapablities(device_capabilities, capability):
-    return device_capabilities & capability == capability
+import pifi.nm_helper as nm
+import pifi.var_io as var_io
 
 def status():
     ApModeDevice = NetworkManager.Device
 
-    for device in NetworkManager.NetworkManager.GetDevices():
-        if device.DeviceType != NetworkManager.NM_DEVICE_TYPE_WIFI:
-            continue
+    for device in nm.managedAPCapableDevices():
         wi_device = device.SpecificDevice()
-        supports_ap = checkCapablities(wi_device.WirelessCapabilities,
-            NetworkManager.NM_WIFI_DEVICE_CAP_AP)
-        if (supports_ap == True):
-            print("Network Mangager reports AP mode support on %s" % wi_device.HwAddress)
-            ApModeDevice = device
+        print("Network Mangager reports AP mode support on %s" % wi_device.HwAddress)
+        ApModeDevice = device
+        current_connection = ApModeDevice.GetAppliedConnection(0)
+        if current_connection[0]['802-11-wireless']['mode'] == "ap":
+            print("Device is currently acting as an Access Point")
+        else:
+            ssid = current_connection[0]['802-11-wireless']['ssid']
+            ssid = bytearray([ord(byte) for byte in ssid])
+            print("Device is connected to %s" % ssid.decode("utf-8"))
 
     if (ApModeDevice == NetworkManager.Device):
         print("ERROR: Network Manager reports no AP mode support on any managed device")
         exit(2)
-    
-    current_connection = ApModeDevice.GetAppliedConnection(0)
-    if current_connection[0]['802-11-wireless']['mode'] == "ap":
-        print("Device is currently acting as an Access Point")
-    else:
-        print("Device is connected to %s" % ''.join(current_connection[0]['802-11-wireless']['ssid']))
 
 def add(ssid, password):
-    if not os.path.isfile("/etc/pifi_pending"):
-        try:
-            with open('/etc/pifi_pending', 'w+') as pending_file:
-                json.dump([], pending_file)
-        except IOError as e:
-            print("Error creating /etc/pifi_pending, make sure you are running with sudo") 
-
     try:
-        with open('/etc/pifi_pending', 'r+') as pending_file:    
-            pending = json.load(pending_file)
-            pending.append({'ssid' : ssid, 'password' : password})
-            pending_file.seek(0)  # rewind
-            json.dump(pending, pending_file)
-            pending_file.truncate()
-    except IOError as e:
-        print("Error appending to /etc/pifi_pending, make sure you are running with sudo") 
+        pending = var_io.readPendingConnections()
+        pending.append({'ssid' : ssid, 'password' : password})
+        var_io.writePendingConnections(pending)
+    except PermissionError:
+        print("Error writing to /var/lib/pifi/pending, make sure you are running with sudo")
 
 def list_seen():
     with open('/tmp/seen_ssids') as seen_file:
@@ -75,7 +60,7 @@ def list_pending():
             print(connection['ssid'])
 
 def main():
-    arguments = docopt(__doc__, version='Pifi Version 0.1.3')
+    arguments = docopt(__doc__, version='pifi version 0.1.3')
     
     if arguments['status']:
         status()
