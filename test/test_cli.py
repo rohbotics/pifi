c0875499b79121e22815b77cd51dbd611452be8b
Rohan Agrawal <send2arohan@gmail.com>
2017-08-22 18:56:38 -0700

    add append connection test to pifi cli

--- test/test_cli.py
+++ test/test_cli.py
@@ -76,6 +76,42 @@ class NMHelperTests(unittest.TestCase):
 
         pifi.add('Foo', 'bar', var_io=var) # Only checking for no exceptions
 
+    def test_add_one_secure_connection_existing_list(self):
+        existing_connection = {'Baz' : 'qux'}
+
+        var = mock.MagicMock()
+        var.configure_mock(**{'readPendingConnections.return_value': [existing_connection]})
+
+        pifi.add('Foo', 'bar', var_io=var)
+
+        var.readPendingConnections.assert_called_once_with()
+        written_connections = var.writePendingConnections.call_args[0][0]
+        del written_connections[1]['connection']['uuid']
+
+        expected_connection = {
+            'connection': {
+                'id': 'Foo',
+                'type': '802-11-wireless',
+                'autoconnect': True,
+            },
+
+            '802-11-wireless': {
+                'mode': 'infrastructure',
+                'security': '802-11-wireless-security',
+                'ssid': 'Foo'
+            },
+
+            '802-11-wireless-security': {
+                'key-mgmt': 'wpa-psk', # We only support WPA2-PSK networks for now
+                'psk': 'bar'
+            },
+
+            'ipv4': {'method': 'auto'},
+            'ipv6': {'method': 'auto'}
+        }
+        
+        self.assertEqual(written_connections, [existing_connection, expected_connection])
+
 
 def main():
     unittest.main()
