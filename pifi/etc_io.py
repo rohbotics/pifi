227cc009d206e1903b00935df00307d4f60716d3
Rohan Agrawal <send2arohan@gmail.com>
2017-09-03 11:57:07 -0700

    Support a yaml conf file, better testing for etc_io

--- pifi/etc_io.py
+++ pifi/etc_io.py
@@ -5,39 +5,76 @@ For now this is just the default AP configuration
 """
 
 default_ap_path = "/etc/pifi/default_ap.em"
-#default_ap_path = "../default_ap.em"
+conf_path = "/etc/pifi/pifi.conf"
+
+default_ap_path = "../default_ap.em"
+conf_path = "../pifi.conf"
 
 import os
 import em
-import json
+import json, yaml
 import uuid
 
 def get_default_ap_conf(mac, open=open):
+    fallback_ap_conf = \
+    {
+        'connection': {
+            'id': 'Pifi AP Mode',
+            'type': '802-11-wireless',
+            'autoconnect': False,
+            'uuid': str(uuid.uuid4())
+        },
+
+        '802-11-wireless': {
+            'mode': 'ap',
+            'security': '802-11-wireless-security',
+            'ssid': 'UbiquityRobot%4s' % mac.replace(":", "")[-4:]
+        },
+
+        '802-11-wireless-security': {
+            'key-mgmt': 'wpa-psk',
+            'psk': 'robotseverywhere'
+        },
+
+        'ipv4': {'method': 'shared'},
+        'ipv6': {'method': 'ignore'}
+    }
+
     try:
         with open(default_ap_path) as ap_conf_file:
             ap_conf = ap_conf_file.read()
             expanded_ap_conf = em.expand(ap_conf, {"mac": mac, "uuid_str": str(uuid.uuid4())})
             return json.loads(expanded_ap_conf)
     except FileNotFoundError:
-        return {
-            'connection': {
-                'id': 'Pifi AP Mode',
-                'type': '802-11-wireless',
-                'autoconnect': False,
-                'uuid': str(uuid.uuid4())
-            },
+        print("WARN /etc/pifi/default_ap.em doesn't exist, using fallback configuration")
+        return fallback_ap_conf
+    except (json.decoder.JSONDecodeError, NameError) as e:
+        print("WARN failed to parse /etc/pifi/default_ap.em, using fallback configuration")
+        print(e)
+        return fallback_ap_conf
 
-            '802-11-wireless': {
-                'mode': 'ap',
-                'security': '802-11-wireless-security',
-                'ssid': 'UbiquityRobot%4s' % mac.replace(":", "")[-4:]
-            },
 
-            '802-11-wireless-security': {
-                'key-mgmt': 'wpa-psk',
-                'psk': 'robotseverywhere'
-            },
+default_conf = \
+{
+    'delete_existing_ap_connections' : False
+}
 
-            'ipv4': {'method': 'shared'},
-            'ipv6': {'method': 'ignore'}
-        }
+def get_conf(open=open):
+    try:
+        with open(conf_path) as conf_file:
+            conf = yaml.load(conf_file)
+            if (conf is None):
+                print('WARN /etc/pifi/pifi.conf is empty, using default configuration')
+                return default_conf
+
+            for key, value in default_conf.items():
+                if key not in conf:
+                    conf[key] = value
+
+            return conf
+    except FileNotFoundError:
+        print("WARN /etc/pifi/pifi.conf doesn't exist, using default configuration")
+        return default_conf
+    except yaml.parser.ParserError:
+        print("WARN failed to parse /etc/pifi/pifi.conf, using default configuration")
+        return default_conf
--- test/test_etc_io.py
+++ test/test_etc_io.py
@@ -2,12 +2,24 @@ import unittest
 from unittest import mock
 import pifi.etc_io as etc_io
 import os
-import json
+import json, yaml
 
 class EtcIOTests(unittest.TestCase):
 
     def test_nonexistant_get_default_ap_conf(self):
         f = mock.Mock(side_effect=FileNotFoundError('foo'))
+        conf = etc_io.get_default_ap_conf('AF:BF:CF:0F:1F:2F', open=f)
+        self.assertIsInstance(conf, dict)
+        self.assertEqual(conf['802-11-wireless']['mode'], 'ap')
+
+    def test_bad_json_get_default_ap_conf(self):
+        f = mock.mock_open(read_data='{"Foo" : f"')
+        conf = etc_io.get_default_ap_conf('AF:BF:CF:0F:1F:2F', open=f)
+        self.assertIsInstance(conf, dict)
+        self.assertEqual(conf['802-11-wireless']['mode'], 'ap')
+
+    def test_bad_template_get_default_ap_conf(self):
+        f = mock.mock_open(read_data='{"Foo" : "@(foo)"}')
         conf = etc_io.get_default_ap_conf('AF:BF:CF:0F:1F:2F', open=f)
         self.assertIsInstance(conf, dict)
         self.assertEqual(conf['802-11-wireless']['mode'], 'ap')
@@ -24,7 +36,36 @@ class EtcIOTests(unittest.TestCase):
         self.assertIsInstance(conf, dict)
         self.assertEqual(conf, {'Foo' : 'AF:BF:CF:0F:1F:2F'})
 
+    def test_nonexistant_conf(self):
+        f = mock.Mock(side_effect=FileNotFoundError('foo'))
+        conf = etc_io.get_conf(open=f)
+        self.assertIsInstance(conf, dict)
 
+    def test_good_conf(self):
+        f = mock.mock_open(read_data=yaml.dump({'delete_existing_ap_connections' : True}))
+        conf = etc_io.get_conf(open=f)
+        self.assertIsInstance(conf, dict)
+        self.assertEqual(conf, {'delete_existing_ap_connections' : True})
+
+    def test_incomplete_conf(self):
+        f = mock.mock_open(read_data=yaml.dump({'Foo' : 'Bar'}))
+        conf = etc_io.get_conf(open=f)
+        self.assertIsInstance(conf, dict)
+        self.assertEqual(conf, {'delete_existing_ap_connections' : False, 'Foo': 'Bar'})
+
+    def test_empty_conf(self):
+        f = mock.mock_open(read_data='')
+        conf = etc_io.get_conf(open=f)
+        self.assertIsNot(conf, None)
+        self.assertIsInstance(conf, dict)
+        self.assertEqual(conf, etc_io.default_conf)
+
+    def test_bad_conf(self):
+        f = mock.mock_open(read_data=': fd')
+        conf = etc_io.get_conf(open=f)
+        self.assertIsNot(conf, None)
+        self.assertIsInstance(conf, dict)
+        self.assertEqual(conf, etc_io.default_conf)
 
 def main():
     unittest.main()
