82250e59aad7350e0fe1b869f5b53566c97fe20d
Ulysse Vautier <ulysse.vautier@plymouth.ac.uk>
2019-11-10 14:27:02 +0000

    Fix status issue with only mode 'ap'
    The check for 'ap' is done both whether mode is in current_connection and if mode == 'ap'.

--- pifi/pifi.py
+++ pifi/pifi.py
@@ -71,9 +71,8 @@ def status(nm=nm):
             print("Device is not activated")
             exit(0)
         current_connection = ApModeDevice.GetAppliedConnection(0)
-        if 'mode' in current_connection[0]['802-11-wireless']:
-            if current_connection[0]['802-11-wireless']['mode'] == "ap":
-                print("Device is currently acting as an Access Point")
+        if 'mode' in current_connection[0]['802-11-wireless'] and current_connection[0]['802-11-wireless']['mode'] == "ap":
+            print("Device is currently acting as an Access Point")
         else:
             ssid = current_connection[0]['802-11-wireless']['ssid']
             ssid = bytearray([ord(byte) for byte in ssid])
