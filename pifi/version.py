7b71584b4b38d435b75729edcce778f6e93e3c1a
Rohan Agrawal <send2arohan@gmail.com>
2021-06-08 10:04:09 -0700

    Get version info from a common place always, makes releases easier

---
+++ pifi/version.py
@@ -0,0 +1,3 @@
+# Store the version info in one place
+# Inspired by https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
+__version__ = "0.10.0"
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
+exec(open("pifi/version.py").read())
+
 setup(
     name="pifi",
-    version="0.9.1",
+    version=__version__,
     description="Wifi provisioning tools for robots with Raspberry Pis",
     url="https://github.com/rohbotics/pifi",
     author="Rohan Agrawal",
@@ -11,7 +13,10 @@ setup(
     packages=find_packages(exclude=["test"]),
     install_requires=["python-networkmanager", "empy", "pyyaml", "evdev"],
     entry_points={
-        "console_scripts": ["pifi_startup=pifi.startup:main", "pifi=pifi.pifi:main",],
+        "console_scripts": [
+            "pifi_startup=pifi.startup:main",
+            "pifi=pifi.pifi:main",
+        ],
     },
     test_suite="test",
 )
